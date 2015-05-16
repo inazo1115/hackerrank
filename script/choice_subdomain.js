385340daf3d7406bd4fd4eb02376bc75d58661dd
inazo <inazo1115@gmail.com>
2015-05-17 07:44:59 +0900

    add script

---
+++ script/choice_subdomain.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var subdomains = [
+  "warmup",
+  "strings",
+  "sorting",
+  "search",
+  "data_structures",
+  "graph_theory",
+  "greedy",
+  "dynamic_programming",
+  "bit_manipulation",
+  "regex",
+  "number_theory",
+  "combinatorics",
+  "summations_and_algebra",
+  "geometry",
+  "probability",
+  "game_theory",
+  "np_complete",
+  "cryptography",
+  "code_golf",
+  "multiple_choice"
+];
+
+/**
+ * chice subdomain from algorithms.
+ */
+(function() {
+  var n = Math.floor(subdomains.length * Math.random());
+  console.log(subdomains[n]);
+})();
+
