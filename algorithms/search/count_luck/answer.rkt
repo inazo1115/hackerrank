de8532d6eb918fb56f6c55c0f9120d575379b61b
inazo <inazo1115@gmail.com>
2015-08-16 17:59:50 +0900

    success

--- algorithms/search/count_luck/answer.rkt
+++ algorithms/search/count_luck/answer.rkt
@@ -2,10 +2,49 @@
 
 (struct point (x y) #:prefab)
 
+(define (get f x y)
+  (vector-ref (vector-ref f x) y)
+  )
+
 ; (number, number, vector[vector[string]], point, point) -> number
-(define (solve n m f now gorl)
-  (vector-set! (vector-ref f (point-x now)) (point-y now) "X")
+(define (solve n m f now gorl k val)
   
+  (when (equal? (get f (point-x now) (point-y now)) "*")
+    (displayln (if (eqv? k val)
+                   "Impressed"
+                   "Oops!"
+                   )))
+
+  (vector-set! (vector-ref f (point-x now)) (point-y now) "X")
+  ; up
+  (define up? (and (>= (sub1 (point-x now)) 0)
+                   (not (equal? (get f (sub1 (point-x now)) (point-y now)) "X"))
+                   ))
+  ; right
+  (define right? (and (< (add1 (point-y now)) m)
+                      (not (equal? (get f (point-x now) (add1 (point-y now))) "X"))
+                      ))
+  ; down
+  (define down? (and (< (add1 (point-x now)) n)
+                     (not (equal? (get f (add1 (point-x now)) (point-y now)) "X"))
+                     ))
+  ; left
+  (define left? (and (>= (sub1 (point-y now)) 0)
+                     (not (equal? (get f (point-x now) (sub1 (point-y now))) "X"))
+                     ))
+
+  (define num (length (filter (lambda (p) p)
+                              (list up? right? down? left?))
+                      ))
+  (when (> num 1)
+    (set! val (add1 val))
+    )
+
+  (when up?    (solve n m f (point (sub1 (point-x now)) (point-y now)) gorl k val))
+  (when right? (solve n m f (point (point-x now) (add1 (point-y now))) gorl k val))
+  (when down?  (solve n m f (point (add1 (point-x now)) (point-y now)) gorl k val))
+  (when left?  (solve n m f (point (point-x now) (sub1 (point-y now))) gorl k val))
+
   )
 
 ; (number, number, vector[vector[string]], string) -> point
@@ -29,9 +68,8 @@
   (define K (string->number (read-line)))
   (define start (find N M f "M"))
   (define end (find N M f "*"))
-  (displayln (if (eqv? (solve N M f start end) K)
-                 "Impressed"
-                 "Oops!")))
+  (solve N M f start end K 0)
+  )
 
 (define T (string->number (read-line)))
 (for-each (lambda (_) (process))
