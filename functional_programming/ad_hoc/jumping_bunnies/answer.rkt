ff81bb90387f4275ff2a0f1d32734508819b0bef
inaba <inaba@comp.is.uec.ac.jp>
2015-09-17 00:54:19 +0900

    success

--- functional_programming/ad_hoc/jumping_bunnies/answer.rkt
+++ functional_programming/ad_hoc/jumping_bunnies/answer.rkt
@@ -1,43 +1,18 @@
 #lang racket
 
-(define (solve xs)
-  (if (null? xs)
-      '()
-      (cons (car xs) (solve (my-filter (car xs) (cdr xs))))))
+(define (gcd x y)
+  (if (zero? y)
+      x
+      (gcd y (modulo x y))))
 
-;(2 3) ((2 2) (5 7))
-(define (my-filter x xs)
-  (map (lambda (y) (bar x y))
-       xs))
-
-;(2 3) (2 2) -> (2), (5 7) (2 2) -> (2 2)
-(define (bar x y)
-  (cond [(null? x) y]
-        [(null? y) '()]
-        [(eqv? (car x) (car y)) (bar (cdr x) (cdr y))]
-        [else (bar (cdr x) y)]))
-
-; number -> list[number]
-(define (prime-fact x)
-  (define (helper n primes)
-    (cond [(eqv? n 1) '()]
-          [(zero? (modulo n (car primes)))
-           (cons (car primes) (helper (quotient n (car primes)) primes))]
-          [else (helper n (cdr primes))]))
-  (helper x (sieve x)))
-
-; number -> list[number]
-(define (sieve x)
-  (define (helper xs)
-    (if (null? xs)
-        '()
-        (cons (car xs) (helper (filter (lambda (e) (positive? (modulo e (car xs))))
-                                       xs)))))
-  (helper (range 2 (add1 x))))
+(define (lcm x y)
+  (/ (* x y) (gcd x y)))
 
 ; main
 (void (read-line))
-(define xs (filter positive?
-                   (map string->number
-                        (string-split (read-line) " "))))
-(apply * (flatten (solve (map prime-fact xs))))
+(define xs (map string->number
+                (string-split (read-line) " ")))
+
+(foldr (lambda (e v) (lcm e v))
+       1
+       xs)
