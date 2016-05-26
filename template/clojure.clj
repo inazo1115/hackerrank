838f7e3c42132b3db18a621007bdbaba17ae9df3
inazo1115 <inaba1115@gmail.com>
2016-05-26 21:44:17 +0900

    Update

--- template/clojure.clj
+++ template/clojure.clj
@@ -1,11 +1,10 @@
 ;; require
-(require '[clojure.data :as dat]
-         '[clojure.string :as str])
+(require '[clojure.string :as str])
 
 ;; read
 (defn read-line-lst [] (map read-string (str/split (read-line) #" ")))
-(defn read-word-lst ([n] (if (zero? n) '()
-                             (cons (read) (read-word-lst (dec n))))))
+(defn read-word-lst [n] (if (zero? n) '()
+                            (cons (read) (read-word-lst (dec n)))))
 
 ;; solve
 (defn solve [] "Write your answer here.")
